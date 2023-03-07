c87c656c5cb6ef3f638cc78577b1c80fec63ad4d
smlee212 <smmylee212@naver.com>
2023-03-07 14:26:47 +0900

    init setting

---
+++ BackEnd/src/main/java/com/ssafy/bbkk/BbkkApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bbkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbkkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbkkApplication.class, args);
+	}
+
+}
