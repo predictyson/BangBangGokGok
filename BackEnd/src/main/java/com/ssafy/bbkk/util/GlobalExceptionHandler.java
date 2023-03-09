3d811d69d33ee8ad097fefc9f9a6281073d0eda9
smmylee212@naver.com <smmylee212>
2023-03-09 11:19:34 +0900

    Add : 전역 예외처리 핸들러 추가

---
+++ BackEnd/src/main/java/com/ssafy/bbkk/util/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.ssafy.bbkk.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice(basePackages = "com.ssafy.bbkk.api.controller")
+@Slf4j
+public class GlobalExceptionHandler{
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleAllException(Exception e) {
+        log.warn("handleAllException", e);
+        Map<String, Object> resultMap = new HashMap<>();
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        resultMap.put("error", e.getMessage());
+        return new ResponseEntity<Map<String, Object>>(resultMap, status);
+    }
+}
