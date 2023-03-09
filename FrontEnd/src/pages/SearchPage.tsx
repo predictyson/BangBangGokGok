89b74f7e02c8a4f626ba2f3ba22f79d3c72f786f
benkim077 <123vfcx@gmail.com>
2023-03-09 17:55:24 +0900

    🎨 style: SearchPage 레이아웃 작업 진행

--- FrontEnd/src/pages/SearchPage.tsx
+++ FrontEnd/src/pages/SearchPage.tsx
@@ -1,21 +1,42 @@
 import React from "react";
-import Container from "@mui/material/Container";
 import SearchInput from "@components/search/SearchInput";
 import SearchSortOptions from "@components/search/SearchSortOptions";
 import SearchFilter from "@components/search/SearchFilter";
 import SearchResult from "@components/search/SearchResult";
 import Header from "@components/common/Header";
+import styled from "styled-components";
+import { theme } from "@/styles/theme";
 
 export default function SearchPage() {
   return (
     <>
       <Header />
-      <Container>
-        <SearchInput />
-        <SearchSortOptions />
-        <SearchFilter />
-        <SearchResult />
-      </Container>
+      <BackGround>
+        <Content>
+          <SearchInput />
+          <SearchSortOptions />
+          <SearchFilter />
+          <SearchResult />
+        </Content>
+      </BackGround>
     </>
   );
 }
+
+const BackGround = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${theme.colors.background};
+`;
+
+const Content = styled.div`
+  width: 79%;
+  margin: 2rem auto;
+  padding: 0px 8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: ${theme.colors.containerLight};
+  border-radius: 1.5rem;
+`;
