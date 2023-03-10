3334bd983c7b5c301e59d76b7cdd2cd4b3e23990
benkim077 <123vfcx@gmail.com>
2023-03-10 16:57:10 +0900

    ✨ feat: SearchInput 검색창 스타일링

--- FrontEnd/src/components/search/SearchInput.tsx
+++ FrontEnd/src/components/search/SearchInput.tsx
@@ -2,39 +2,38 @@ import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { styled as mstyled } from "@mui/material/styles";
+import styled from "styled-components";
 
 export default function SearchInput() {
   const [searchText, setSearchText] = useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  };
+
+  console.log(searchText);
+
   return (
-    <Box component="div">
-      <CustomTextField fullWidth label="검색어를 입력하세요.">
-        {searchText}
-      </CustomTextField>
-    </Box>
+    <Container>
+      <Input onChange={handleChange} placeholder="검색어를 입력하세요." />
+    </Container>
   );
 }
 
-const CustomTextField = mstyled(TextField)({
-  "& .MuiInputBase-root": {
-    color: "white",
-    backgroundColor: "transparent",
-    borderRadius: "20px",
-    border: "1px solid white",
-    "&:hover, &.Mui-focused": {
-      backgroundColor: "rgba(255, 255, 255, 0.1)",
-      border: "1px solid white",
-    },
-    "&:after": {
-      borderColor: "white",
-    },
-  },
-  "& .MuiInputLabel-root": {
-    color: "white",
-    "&.Mui-focused": {
-      color: "white",
-    },
-  },
-  "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-    borderColor: "white",
-  },
-});
+const Container = styled.div`
+  width: 50rem;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 5rem;
+  padding: 0 1rem;
+  font-size: 2rem;
+  border-radius: 20px;
+  border: 1px solid white;
+  background-color: transparent;
+  color: white;
+  &:focus {
+    outline: none;
+  }
+`;
