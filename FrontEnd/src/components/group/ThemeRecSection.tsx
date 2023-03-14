9f1a16f7d5613eccc13fa5f9bbc34660b758857e
정상기 <jackid1103@naver.com>
2023-03-14 17:42:09 +0900

    ✨ feat: 그룹셋 추천 결과 타이핑 대기 애니메이션 추가

--- FrontEnd/src/components/group/ThemeRecSection.tsx
+++ FrontEnd/src/components/group/ThemeRecSection.tsx
@@ -1,15 +1,44 @@
 import { theme } from "@/styles/theme";
-import React from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled, { keyframes } from "styled-components";
 
 export default function ThemeRecSection({
   userList,
 }: {
   userList: GroupSetUer[];
 }) {
+  const [recTheme, setRectTheme] = useState<IThemeData[]>([]);
+  const [isShow, setIsShow] = useState<boolean>(false);
+  const [isWaiting, setIsWaiting] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isWaiting) {
+      setTimeout(() => {
+        setIsShow(true);
+        setIsWaiting(false);
+      }, 5000);
+    }
+  }, [isWaiting]);
+
+  const handleResult = () => {
+    setIsWaiting(true);
+  };
+
   return (
     <Container>
-      <ResultButton>결가 부기</ResultButton>
+      {!isShow ? (
+        <>
+          {isWaiting ? (
+            <WaitAnimation>
+              <b>이상해씨</b> 님의 그룹의 맞춤 추천 테마를 찾는중입니다......
+            </WaitAnimation>
+          ) : (
+            <ResultButton onClick={handleResult}>결과 보기</ResultButton>
+          )}
+        </>
+      ) : (
+        "result"
+      )}
     </Container>
   );
 }
@@ -37,4 +66,23 @@ const ResultButton = styled.div`
   &:hover {
     transform: scale(1.1);
   }
+`;
+const typing = keyframes`
+  from {
+    width: 0;
+  }
+  to {
+    width: 100%;
+  }
+`;
+
+const WaitAnimation = styled.div`
+  margin: 0 auto;
+  text-align: center;
+  font-size: 3rem;
+  padding: 1rem;
+  white-space: nowrap;
+  color: ${theme.colors.pink};
+  overflow: hidden; /* 요소 내의 내용이 넘치는 경우 가려진 부분은 보이지 않게 함 */
+  animation: ${typing} 5s steps(60, end); /* 5초 동안 한글자씩 타이핑되는 애니메이션 */
 `;
