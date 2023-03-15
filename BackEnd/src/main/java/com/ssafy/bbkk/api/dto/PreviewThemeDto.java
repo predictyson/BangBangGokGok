de2f935fee9375356f7fcfeff5c002185654b484
smmylee212@naver.com <smmylee212>
2023-03-15 15:51:52 +0900

    ✨ feat: 테마 장르 목록 추가

--- BackEnd/src/main/java/com/ssafy/bbkk/api/dto/PreviewThemeDto.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/dto/PreviewThemeDto.java
@@ -4,6 +4,8 @@ import com.ssafy.bbkk.db.entity.Theme;
 import lombok.ToString;
 
 import java.sql.Clob;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ToString
 public class PreviewThemeDto {
@@ -11,10 +13,15 @@ public class PreviewThemeDto {
     private int themeId; // 테마 id
     private String title; // 테마명
     private Clob imgUrl; // 테마 포스터 링크
+    private List<String> genres; // 테마 장르 목록
 
     public PreviewThemeDto(Theme theme) {
         this.themeId = theme.getId();
         this.title = theme.getTitle();
         this.imgUrl = theme.getImgUrl();
+        this.genres = theme.getGenreOfThemes()
+                .stream()
+                .map(x->x.getGenre().getCategory())
+                .collect(Collectors.toList());
     }
 }
