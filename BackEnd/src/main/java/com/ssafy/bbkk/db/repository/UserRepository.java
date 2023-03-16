be4e09e7667d00edd110e06222a55342e3bcd92f
smmylee212@naver.com <smmylee212>
2023-03-16 13:29:34 +0900

    💡 refactor: 유저 검색 api에서 입력한 값을 포함하는 유저들을 반환하도록 변경

--- BackEnd/src/main/java/com/ssafy/bbkk/db/repository/UserRepository.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/db/repository/UserRepository.java
@@ -3,10 +3,12 @@ package com.ssafy.bbkk.db.repository;
 import com.ssafy.bbkk.db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
-    Optional<User> findByEmailOrNickname(String email, String nickname);
+    List<User> findByEmailContaining(String email);
+    List<User> findByNicknameContaining(String nickname);
     void deleteByEmail(String email);
 }
