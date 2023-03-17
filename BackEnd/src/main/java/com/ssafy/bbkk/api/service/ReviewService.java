25407152d93425351fd76f86b5ba21fde9f505d5
smmylee212@naver.com <smmylee212>
2023-03-17 14:19:25 +0900

    ✨ feat: 리뷰 조회 api 생성

--- BackEnd/src/main/java/com/ssafy/bbkk/api/service/ReviewService.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/service/ReviewService.java
@@ -4,9 +4,12 @@ import com.ssafy.bbkk.api.dto.CreateReviewRequest;
 import com.ssafy.bbkk.api.dto.ReviewOfUserResponse;
 import com.ssafy.bbkk.api.dto.UpdateReviewRequest;
 
+import java.util.List;
+
 public interface ReviewService {
+    ReviewOfUserResponse getReview(int themeId) throws Exception;
+    List<ReviewOfUserResponse> getReviews(int reviewId) throws Exception;
     void addReview(String email, CreateReviewRequest createReviewRequest) throws Exception;
     void deleteReview(String email, int reviewId) throws Exception;
-    ReviewOfUserResponse getReview(int reviewId) throws Exception;
     void setReview(String email, UpdateReviewRequest updateReviewRequest) throws Exception;
 }
