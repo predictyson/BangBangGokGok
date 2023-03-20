227d6b2e1fa99a52264f2d9f2c89fc9191577e9a
smmylee212@naver.com <smmylee212>
2023-03-20 16:24:09 +0900

    ✨ feat: 스프링 시큐리티 초기 설정

---
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/common/auth/PrincipalDetailsService.java
@@ -0,0 +1,43 @@
+package com.ssafy.bbkk.api.common.auth;
+
+import com.ssafy.bbkk.db.entity.User;
+import com.ssafy.bbkk.db.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Collections;
+
+// 시큐리티 설정에서 loginProcessingUrl()
+// login 요청이 오면 자동으로 UserDetailsService 타입으로 IoC 되어 있는 loadUserByUsername 함수가 실행
+@Service
+@RequiredArgsConstructor
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 유저 불러오기
+        return userRepository.findByEmail(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " -> 데이터베이스에서 찾을 수 없습니다."));
+    }
+
+    // DB 에 User 값이 존재한다면 UserDetails 객체로 만들어서 리턴
+    private UserDetails createUserDetails(User user) {
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getRoles());
+
+        return new org.springframework.security.core.userdetails.User(
+                String.valueOf(user.getId()),
+                user.getPassword(),
+                Collections.singleton(grantedAuthority)
+        );
+    }
+}
