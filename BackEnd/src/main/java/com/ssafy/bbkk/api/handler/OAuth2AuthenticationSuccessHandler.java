9d43b1a9240adc9e2c089550524daeca7ec3c29d
smmylee212@naver.com <smmylee212>
2023-03-21 10:55:26 +0900

    💡 refactor: 소셜 로그인과 회원가입을 구분하여 redirect를 나눔

--- BackEnd/src/main/java/com/ssafy/bbkk/api/handler/OAuth2AuthenticationSuccessHandler.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/handler/OAuth2AuthenticationSuccessHandler.java
@@ -1,10 +1,14 @@
 package com.ssafy.bbkk.api.handler;
 
+import com.ssafy.bbkk.api.common.auth.PrincipalDetails;
 import com.ssafy.bbkk.api.common.jwt.TokenProvider;
 import com.ssafy.bbkk.api.dto.TokenResponse;
 import com.ssafy.bbkk.db.entity.RefreshToken;
+import com.ssafy.bbkk.db.entity.User;
 import com.ssafy.bbkk.db.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -20,6 +24,8 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(OAuth2AuthenticationSuccessHandler.class);
+
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
 
@@ -31,7 +37,7 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         String targetUrl = determineTargetUrl(request, response, authentication);
 
         if (response.isCommitted()) {
-            logger.debug("Response has already been committed. Unable to redirect to " + targetUrl);
+            logger.info("Response has already been committed. Unable to redirect to " + targetUrl);
             return;
         }
 
@@ -40,23 +46,51 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
 
     protected String determineTargetUrl(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
 
-        // 소셜 로그인 성공 후 이동할 페이지
-        String targetUrl = "/oauth2/social/success";
+        PrincipalDetails principalDetails = (PrincipalDetails)authentication.getPrincipal();
+        User user = principalDetails.getUser();
 
-        // 3. 인증 정보를 기반으로 JWT 토큰 생성
-        TokenResponse tokenDto = tokenProvider.generateToken(authentication);
+        logger.info("[determineTargetUrl] : principalDetails={}", principalDetails);
 
-        // 4. RefreshToken 저장
-        RefreshToken refreshToken = RefreshToken.builder()
-                .key(authentication.getName())
-                .value(tokenDto.getRefreshToken())
-                .build();
+        // 소셜 로그인 성공 후 이동할 페이지 -> 추후 변경해야함
+        String targetUrl = "/addInfo";
 
-        refreshTokenRepository.save(refreshToken);
+        // 추가 정보가 입력되어 있다면 로그인 처리
+        if(user.getAge()>0 &&
+                ("W".equals(user.getGender()) || "M".equals(user.getGender())) &&
+                user.getNickname()!=null &&
+                user.getProfileImageType()!=null &&
+                user.getRegion()!=null){
 
+            // 토큰 정보 저장하는 페이지로 이동
+            targetUrl = "/social/success";
+
+            // 3. 인증 정보를 기반으로 JWT 토큰 생성
+            String accessToken = tokenProvider.generateAccessToken(authentication);
+            String refreshToken = tokenProvider.generateRefreshToken();
+
+            TokenResponse tokenDto = TokenResponse.builder()
+                    .accessToken(accessToken)
+                    .refreshToken(refreshToken)
+                    .build();
+
+            // 4. RefreshToken 저장
+            RefreshToken rfToken = RefreshToken.builder()
+                    .key(authentication.getName())
+                    .value(tokenDto.getRefreshToken())
+                    .build();
+
+            refreshTokenRepository.save(rfToken);
+
+            // 타겟 URL로 토큰 정보를 함께 보내줌
+            return UriComponentsBuilder.fromUriString(targetUrl)
+                    .queryParam("accessToken", tokenDto.getAccessToken())
+                    .queryParam("refreshToken", tokenDto.getRefreshToken())
+                    .build().toUriString();
+        }
+
+        // 추가 정보가 입력되어 있지 않다면 추가 정보 입력창으로 보냄
         return UriComponentsBuilder.fromUriString(targetUrl)
-                .queryParam("accessToken", tokenDto.getAccessToken())
-                .queryParam("refreshToken", tokenDto.getRefreshToken())
+                .queryParam("email", user.getEmail())
                 .build().toUriString();
     }
 }
