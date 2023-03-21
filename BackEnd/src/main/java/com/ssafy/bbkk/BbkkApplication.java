a896ffcb4b1137000159005fa79160e8e41e2a2a
smmylee212@naver.com <smmylee212>
2023-03-21 15:30:25 +0900

    💡 refactor: basetime entity가 자동 생성할 수 있도록 변경

--- BackEnd/src/main/java/com/ssafy/bbkk/BbkkApplication.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/BbkkApplication.java
@@ -2,12 +2,19 @@ package com.ssafy.bbkk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import java.util.Locale;
+import java.util.TimeZone;
+
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class BbkkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BbkkApplication.class, args);
 	}
-
 }
