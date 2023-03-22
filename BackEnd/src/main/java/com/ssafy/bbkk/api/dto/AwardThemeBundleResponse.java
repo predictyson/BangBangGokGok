9543e1ab6c89c177e8481bae96452c673990c1c6
Sangbin <qlsqlsz1@naver.com>
2023-03-22 10:49:44 +0900

    💡 refactor : AwardThemeBundleResponse 수정

--- BackEnd/src/main/java/com/ssafy/bbkk/api/dto/AwardThemeBundleResponse.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/dto/AwardThemeBundleResponse.java
@@ -1,13 +1,21 @@
 package com.ssafy.bbkk.api.dto;
 
+import com.ssafy.bbkk.db.entity.AwardTheme;
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.ToString;
 
 @ToString
 public class AwardThemeBundleResponse {
 
-    private String label; // 수상 테마 목록의 라벨
-    private String year; // 수상 연도
-    private List<PreviewThemeResponse> theme; // 수상 테마 목록
+    private int year; // 어워즈 연도
+    private List<AwardThemeResponse> theme; // 수상 테마 목록
+
+    public AwardThemeBundleResponse(int year, List<AwardTheme> list) {
+        this.year = year;
+        this.theme = list.stream()
+                .map(x -> new AwardThemeResponse(x))
+                .collect(Collectors.toList());
+    }
 
 }
