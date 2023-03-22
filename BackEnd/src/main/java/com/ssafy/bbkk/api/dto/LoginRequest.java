60e4c258273c25a068fff0c701b659218d6b8ed4
smmylee212@naver.com <smmylee212>
2023-03-22 13:21:42 +0900

    ✨ feat: swagger에 request dto 설명 추가

--- BackEnd/src/main/java/com/ssafy/bbkk/api/dto/LoginRequest.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/dto/LoginRequest.java
@@ -1,14 +1,18 @@
 package com.ssafy.bbkk.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 @ToString
 @Getter
+@Schema(description = "로그인 정보")
 public class LoginRequest {
-    private String email; // 이메일
-    private String password; // 비밀번호
+    @Schema(description = "이메일", required = true)
+    private String email;
+    @Schema(description = "비밀번호", required = true)
+    private String password;
 
     public UsernamePasswordAuthenticationToken toAuthentication(){
         return new UsernamePasswordAuthenticationToken(email, password);
--- BackEnd/src/main/java/com/ssafy/bbkk/api/dto/UpdateUserInfoRequest.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/dto/UpdateUserInfoRequest.java
@@ -1,5 +1,6 @@
 package com.ssafy.bbkk.api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -7,16 +8,25 @@ import java.util.List;
 
 @Getter
 @ToString
+@Schema(description = "유저 프로필 수정 정보")
 public class UpdateUserInfoRequest {
 
-    private int userId; // 유저 id
-    private String nickname; // 수정한 닉네임
-    private String regionBig; // 수정한 선호 지역(대분류)
-    private String regionSmall; // 수정한 선호 지역(소분류)
-    private int age; // 수정한 나이
-    private String gender; // 수정한 성별
-    private String profileImageType; // 수정한 프로필 이미지 타입
-
-    private List<Integer> genreIdAdd; // 추가한 선호 장르 id 목록
-    private List<Integer> genreIdDel; // 삭제한 선호 장르 id 목록
+    @Schema(description = "유저 id", required = true)
+    private int userId;
+    @Schema(description = "닉네임", required = true)
+    private String nickname;
+    @Schema(description = "선호 지역(대분류)", required = true)
+    private String regionBig;
+    @Schema(description = "선호 지역(소분류)", required = true)
+    private String regionSmall;
+    @Schema(description = "나이", required = true)
+    private int age;
+    @Schema(description = "성별", required = true)
+    private String gender;
+    @Schema(description = "프로필 이미지 타입", required = true)
+    private String profileImageType;
+    @Schema(description = "추가한 선호 장르 id 목록", required = true)
+    private List<Integer> genreIdAdd;
+    @Schema(description = "삭제한 선호 장르 id 목록", required = true)
+    private List<Integer> genreIdDel;
 }
