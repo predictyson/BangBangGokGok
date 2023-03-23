98f9cc7a07e1fd0a1245cc519860769486ce74c2
leejiwon <won64312000@naver.com>
2023-03-23 14:35:38 +0900

    🧪 test: jwt api 엔드포인트 확인

--- BackEnd/src/main/java/com/ssafy/bbkk/api/controller/JwtController.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/controller/JwtController.java
@@ -3,6 +3,7 @@ package com.ssafy.bbkk.api.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class JwtController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
+    @CrossOrigin("*")
     @GetMapping("/jwt")
-    public String getJwtToken(@CookieValue(name = "jwt", required = false) String jwt) {
-        logger.info("[login] response : token={}", jwt);
+    public String getJwtToken(@CookieValue(name = "access", required = false) String access) {
+        logger.info("[login] response : token={}", access);
 
-        return jwt;
+        return access;
     }
 }
