b72b4263df38abc90fb925a350ef672e4a116d63
leejiwon <won64312000@naver.com>
2023-03-23 15:07:37 +0900

    🧪 test: allowedHeaders 추가

--- BackEnd/src/main/java/com/ssafy/bbkk/config/WebConfig.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/config/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173","http://bbkk.store:8081", "http://localhost:8081") // 허용할 출처
                 .allowedMethods("GET", "POST") // 허용할 HTTP method
+                .allowedHeaders("*")
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
     }
