b4ca0d6fcd6c60d483e40098dfff184b182ca1de
smmylee212@naver.com <smmylee212>
2023-03-23 17:33:41 +0900

    ✨ feat: 관심 테마 조회 api를 작성

--- BackEnd/src/main/java/com/ssafy/bbkk/api/service/InterestThemeService.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/service/InterestThemeService.java
@@ -1,6 +1,7 @@
 package com.ssafy.bbkk.api.service;
 
 public interface InterestThemeService {
+    boolean isInterestTheme(String email, int themeId) throws Exception;
     void addInterestTheme(String email, int themeId) throws Exception;
     void deleteInterestTheme(String email, int themeId) throws Exception;
 }
