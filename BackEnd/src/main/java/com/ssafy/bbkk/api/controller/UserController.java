59544894229c3491e0c9a8ca9614615ead3e4089
leejiwon <won64312000@naver.com>
2023-03-23 14:50:44 +0900

    🧪 test: CrossOrigin origin 설정

--- BackEnd/src/main/java/com/ssafy/bbkk/api/controller/UserController.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/controller/UserController.java
@@ -26,7 +26,7 @@ public class UserController {
     private final UserService userService;
     private final EmailService emailService;
 
-    @CrossOrigin(origins = "*", allowCredentials = "true")
+    @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
     @Operation(summary = "로그인", description = "로그인을 진행한다")
     @PostMapping("login")
     private ResponseEntity<Map<String, Object>> login(
