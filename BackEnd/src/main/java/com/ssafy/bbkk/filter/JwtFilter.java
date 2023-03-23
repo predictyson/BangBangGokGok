acd09a1c62bf92efe8deb1eed24acd462cd568c3
smmylee212@naver.com <smmylee212>
2023-03-23 15:36:03 +0900

    🎨 style: 공백 제거

--- BackEnd/src/main/java/com/ssafy/bbkk/filter/JwtFilter.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/filter/JwtFilter.java
@@ -31,7 +31,6 @@ public class JwtFilter extends OncePerRequestFilter {
         // 1. Request Header 에서 토큰을 꺼냄
         String jwt = resolveToken(request);
 
-
         // 2. validateToken 으로 토큰 유효성 검사
         // 정상 토큰이면 해당 토큰으로 Authentication 을 가져와서 SecurityContext 에 저장
         if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
@@ -45,10 +44,7 @@ public class JwtFilter extends OncePerRequestFilter {
     // Request Header 에서 토큰 정보를 꺼내오기
     private String resolveToken(HttpServletRequest request) {
 
-
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
-
-        System.out.println(bearerToken);
 
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
             return bearerToken.substring(7);
