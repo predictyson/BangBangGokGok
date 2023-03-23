4a4d7c70105fe0d11d71d19828f8cd80cfe78703
leejiwon <won64312000@naver.com>
2023-03-23 14:04:47 +0900

    🧪 test: jwt log test

--- BackEnd/src/main/java/com/ssafy/bbkk/filter/JwtFilter.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/filter/JwtFilter.java
@@ -1,6 +1,7 @@
 package com.ssafy.bbkk.filter;
 
 import com.ssafy.bbkk.common.jwt.TokenProvider;
+import javax.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -30,6 +31,7 @@ public class JwtFilter extends OncePerRequestFilter {
         // 1. Request Header 에서 토큰을 꺼냄
         String jwt = resolveToken(request);
 
+
         // 2. validateToken 으로 토큰 유효성 검사
         // 정상 토큰이면 해당 토큰으로 Authentication 을 가져와서 SecurityContext 에 저장
         if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
@@ -42,7 +44,18 @@ public class JwtFilter extends OncePerRequestFilter {
 
     // Request Header 에서 토큰 정보를 꺼내오기
     private String resolveToken(HttpServletRequest request) {
+
+        for(Cookie cookie : request.getCookies()){
+            System.out.println(cookie);
+            System.out.println(cookie.getName());
+            System.out.println(cookie.getComment());
+            System.out.println(cookie.getValue());
+        }
+
         String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+
+        System.out.println(bearerToken);
+
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
             return bearerToken.substring(7);
         }
