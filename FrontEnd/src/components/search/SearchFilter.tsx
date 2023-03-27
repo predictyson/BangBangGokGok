243077825ea3a2ba52717e8a2f1f0d55a55b7854
benkim077 <123vfcx@gmail.com>
2023-03-27 22:21:11 +0900

    ✨ feat: 난이도 입력 기능 구현

--- FrontEnd/src/components/search/SearchFilter.tsx
+++ FrontEnd/src/components/search/SearchFilter.tsx
@@ -66,7 +66,10 @@ export default function SearchFilter(props: SearchFilterProps) {
               props.handleDumpFilterGenreCategoryInputValueChange
             }
           />
-          <DifficultyForm />
+          <DifficultyForm
+            filterValue={props.filterValue}
+            handleFilterValueChange={props.handleFilterValueChange}
+          />
           <PeopleForm />
           <TimeForm />
           <ButtonContainer>
