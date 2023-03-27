0cf9f63fb80b674be940b1e6fdd9d68d71a196aa
smmylee212@naver.com <smmylee212>
2023-03-27 17:06:49 +0900

    💡 refactor: 반환할 유저 목록의 개수를 10개로 지정

--- BackEnd/src/main/java/com/ssafy/bbkk/api/service/GroupSetServiceImpl.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/service/GroupSetServiceImpl.java
@@ -15,21 +15,35 @@ import java.util.*;
 public class GroupSetServiceImpl implements GroupSetService {
 
     private final UserRepository userRepository;
+    private final int SIZE_OF_USER_LIST = 10;
 
     @Override
     public List<PreviewUserResponse> getUserListByEmailOrNickname(String emailOrNickname) throws Exception {
         List<PreviewUserResponse> result = new ArrayList<>();
+        List<PreviewUserResponse> temp = new ArrayList<>();
+
         // email로 유저 찾아오기
         userRepository.findByEmailContaining(emailOrNickname)
                 .forEach(x->{
-                    if(x.getNickname()!=null) result.add(new PreviewUserResponse(x));
+                    if(x.getNickname()!=null) temp.add(new PreviewUserResponse(x));
                 });
         // nickname으로 유저 찾아오기
         userRepository.findByNicknameContaining(emailOrNickname)
                 .forEach(x->{
-                    if(x.getNickname()!=null) result.add(new PreviewUserResponse(x));
+                    if(x.getNickname()!=null) temp.add(new PreviewUserResponse(x));
                 });
-        Collections.sort(result, Comparator.comparing(PreviewUserResponse::getNickname));
+        Collections.sort(temp, Comparator.comparing(PreviewUserResponse::getNickname));
+
+        // 상위 n개만 반환
+        if(temp.size()>SIZE_OF_USER_LIST){
+            for(int i=0;i<SIZE_OF_USER_LIST;i++){
+                result.add(temp.get(i));
+            }
+        }
+        else {
+            result = temp;
+        }
+
         return result;
     }
 }
