57fe8c8de5eddf4706ba1562924b082190f8dc32
smmylee212@naver.com <smmylee212>
2023-03-27 11:09:05 +0900

    🧪 test: jwt api 권한 부여

--- BackEnd/src/main/java/com/ssafy/bbkk/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig{
 //                    "/review/**",
 //                    "/groupset/**"
 //                    ).authenticated()
-//             .antMatchers("/jwt/**").authenticated()
+             .antMatchers("/jwt/**").authenticated()
             .anyRequest().permitAll()
 
             // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
