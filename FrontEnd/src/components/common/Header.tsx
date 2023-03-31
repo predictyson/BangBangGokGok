1ccf866139297433256e666fca64116016625d79
정상기 <jackid1103@naver.com>
2023-03-31 10:27:13 +0900

    ⛏ fix: 로그아웃 로직 QA 반영

--- FrontEnd/src/components/common/Header.tsx
+++ FrontEnd/src/components/common/Header.tsx
@@ -4,7 +4,7 @@ import Ghost from "@/assets/common/Ghost.png";
 import Logo from "@/assets/common/Logo.png";
 import { theme } from "@/styles/theme";
 import { styled as mstyled } from "@mui/material/styles";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import { clearUserInfo } from "@/api/api";
 import Toast, { showToast } from "@/components/common/Toast";
@@ -12,6 +12,7 @@ import { myPageLoader } from "@/api/routerLoader";
 
 export default function Header() {
   const navigate = useNavigate();
+  const uselocation = useLocation();
   const isLogin = localStorage.getItem("userId") !== null ? true : false;
   // const username = localStorage.getItem("username");
 
@@ -24,8 +25,12 @@ export default function Header() {
 
   const logout = () => {
     clearUserInfo();
+    if (uselocation.pathname === "/") {
+      location.reload();
+    } else {
+      navigate("/");
+    }
     handleToastClick("success", "성공적으로 로그아웃 되었습니다.");
-    navigate("/");
   };
 
   const routeGroupSet = async () => {
