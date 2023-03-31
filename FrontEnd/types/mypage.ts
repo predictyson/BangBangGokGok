9a4cb740583ea0491b20943b9fac8612da8399cd
김성수 <123vfcx@gmail.com>
2023-03-31 14:05:27 +0900

    ✨ feat: 리뷰 수정, 삭제 기능 개발
    - 타입 설정
    - API 요청 함수

--- FrontEnd/types/mypage.ts
+++ FrontEnd/types/mypage.ts
@@ -80,3 +80,27 @@ export interface PostUserProfileParams {
   genreIdAdd: number[];
   genreIdDel: number[];
 }
+
+export interface PutUserProfileParams {
+  reviewId: number; // 리뷰 id
+  content: string; // 리뷰 내용
+  userRating: number; // 평점
+  userActivity: number; // 체감 활동성
+  userFear: number; // 체감 공포도
+  userDifficulty: number; // 체감 난이도
+  isSuccess: number; // 성공 여부 (0:실패, 1:성공)
+}
+
+// 리뷰 수정
+export interface PutReviewResponse {
+  reviewId: number; // 리뷰 id
+  content: string; // 리뷰 내용
+  userRating: number; // 평점
+  userActivity: number; // 체감 활동성
+  userFear: number; // 체감 공포도
+  userDifficulty: number; // 체감 난이도
+  createTime: Date; // 생성 날짜
+  isSuccess: number; // 성공 여부
+
+  theme: PreviewThemeResponse; // 테마 정보
+}
