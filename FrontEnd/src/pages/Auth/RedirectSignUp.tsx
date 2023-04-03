e05fcb691eb21165595145ae6cb682c678ef5056
정상기 <jackid1103@naver.com>
2023-04-03 15:54:05 +0900

    🧪 test: 롤백

--- FrontEnd/src/pages/Auth/RedirectSignUp.tsx
+++ FrontEnd/src/pages/Auth/RedirectSignUp.tsx
@@ -4,19 +4,16 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 export default function RedirectSignUp() {
   const navigate = useNavigate();
-  // 현재 페이지의 URL에서 query parameter 값을 가져옴
-  const queryString = window.location.search;
+  // 현재 페이지의 URL에서 fragment identifier 값을 가져옴
   const fragment = window.location.hash.substring(1);
-  console.log("queryString", queryString);
-  console.log("fragment", fragment);
-  // query parameter 값에서 userId 부분만 추출
-  const userId = queryString.split("=")[1];
+
+  const userId = fragment.split("&")[0].split("=")[1];
 
   // accessToken 값을 출력
   console.log(userId);
 
   useEffect(() => {
-    // navigate("/additional", { state: { userId: userId } });
+    navigate("/additional", { state: { userId: userId } });
   }, []);
 
   return <CircularProgress />;
