b3ef43c4e50645784d1451bc8d267cc692dbb49e
smmylee212@naver.com <smmylee212>
2023-04-03 10:32:22 +0900

    ⛏ fix: 소셜 로그인 추후 페이지 url 수정

--- BackEnd/src/main/java/com/ssafy/bbkk/handler/OAuth2AuthenticationSuccessHandler.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/handler/OAuth2AuthenticationSuccessHandler.java
@@ -50,11 +50,13 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         User user = principalDetails.getUser();
 
         // 소셜 로그인 성공 후 이동할 페이지 -> 추후 변경해야함
-        String targetUrl = "https://bbkk.store/oauth";
+        String targetUrl = "http://localhost:3000/oauth";
+//        String targetUrl = "https://bbkk.store/oauth";
 //        String targetUrl = "/oauth";
 
         if(user.getEmail() == null) {
-            targetUrl = "https://bbkk.store/login";
+            targetUrl = "http://localhost:3000/login";
+//            targetUrl = "https://bbkk.store/login";
             return UriComponentsBuilder.fromUriString(targetUrl)
                     .queryParam("error", "이메일 동의를 하지 않아 회원가입이 불가능합니다.")
                     .build().toUriString();
@@ -67,7 +69,8 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
                 user.getRegion()!=null){
 
             // 토큰 정보 저장하는 페이지로 이동
-            targetUrl = "https://bbkk.store/oauth2";
+            targetUrl = "http://localhost:3000/oauth2";
+//            targetUrl = "https://bbkk.store/oauth2";
 
             // 3. 인증 정보를 기반으로 JWT 토큰 생성
             String accessToken = tokenProvider.generateAccessToken(authentication);
