623a763650d36bd2c6d75b675cf733647c19b813
정상기 <jackid1103@naver.com>
2023-04-03 15:46:47 +0900

    ⛏ fix: SNS 회원가입 테스트

--- FrontEnd/src/pages/Auth/RedirectSignUp.tsx
+++ FrontEnd/src/pages/Auth/RedirectSignUp.tsx
@@ -4,11 +4,11 @@ import CircularProgress from "@mui/material/CircularProgress";
 
 export default function RedirectSignUp() {
   const navigate = useNavigate();
-  // 현재 페이지의 URL에서 fragment identifier 값을 가져옴
-  const fragment = window.location.hash.substring(1);
+  // 현재 페이지의 URL에서 query parameter 값을 가져옴
+  const queryString = window.location.search;
 
-  // fragment identifier 값에서 accessToken 부분만 추출
-  const userId = fragment.split("&")[0].split("=")[1];
+  // query parameter 값에서 userId 부분만 추출
+  const userId = queryString.split("=")[1];
 
   // accessToken 값을 출력
   console.log(userId);
