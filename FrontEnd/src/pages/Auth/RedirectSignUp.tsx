e05d0a7531274bf1274e47f704f0893191909d6d
정상기 <jackid1103@naver.com>
2023-04-03 15:49:30 +0900

    🧪 test: SNS 회원가입 테스트

--- FrontEnd/src/pages/Auth/RedirectSignUp.tsx
+++ FrontEnd/src/pages/Auth/RedirectSignUp.tsx
@@ -6,7 +6,9 @@ export default function RedirectSignUp() {
   const navigate = useNavigate();
   // 현재 페이지의 URL에서 query parameter 값을 가져옴
   const queryString = window.location.search;
-
+  const fragment = window.location.hash.substring(1);
+  console.log("queryString", queryString);
+  console.log("fragment", fragment);
   // query parameter 값에서 userId 부분만 추출
   const userId = queryString.split("=")[1];
 
@@ -14,7 +16,7 @@ export default function RedirectSignUp() {
   console.log(userId);
 
   useEffect(() => {
-    navigate("/additional", { state: { userId: userId } });
+    // navigate("/additional", { state: { userId: userId } });
   }, []);
 
   return <CircularProgress />;
