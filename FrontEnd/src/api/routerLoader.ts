ab8a81252093b9eae1c5a436aca38ce7794572aa
정상기 <jackid1103@naver.com>
2023-04-05 17:06:08 +0900

    ⛏ fix: routerLoader 수정

--- FrontEnd/src/api/routerLoader.ts
+++ FrontEnd/src/api/routerLoader.ts
@@ -2,9 +2,14 @@ import { requestCheckLoginUser } from "@/api/auth";
 
 export const myPageLoader = async () => {
   const userId = Number(localStorage.getItem("userId"));
-  const {
-    data: { isLoginUser },
-  } = await requestCheckLoginUser(userId);
-  console.log(isLoginUser);
-  return isLoginUser;
+  try {
+    const {
+      data: { isLoginUser },
+    } = await requestCheckLoginUser(userId);
+    console.log(isLoginUser);
+    if (!isLoginUser) return false;
+  } catch (error) {
+    console.log(error);
+  }
+  return true;
 };
