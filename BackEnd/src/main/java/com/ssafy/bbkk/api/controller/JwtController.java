24797aa023d4e4a0994cf3b878abbf018603e602
smmylee212@naver.com <smmylee212>
2023-04-05 11:15:22 +0900

    🧪 test: now 테스트 코드 작성

--- BackEnd/src/main/java/com/ssafy/bbkk/api/controller/JwtController.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/api/controller/JwtController.java
@@ -4,11 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.time.LocalDateTime;
 import java.util.Enumeration;
 import java.util.Set;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+import com.ssafy.bbkk.db.entity.RefreshToken;
+import com.ssafy.bbkk.db.repository.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,10 +27,22 @@ import org.springframework.web.bind.annotation.*;
 public class JwtController {
 
     private static final Logger logger = LoggerFactory.getLogger(JwtController.class);
+    private final RefreshTokenRepository repository;
 
     @GetMapping
     public String getJwtToken(@AuthenticationPrincipal User user, HttpServletRequest request) {
             logger.info("[jwt] user={}",user.getUsername());
         return user.getUsername();
+    }
+
+    @GetMapping("now")
+    public String getTime() {
+        logger.info("now = {}",LocalDateTime.now());
+
+        RefreshToken refreshToken = new RefreshToken("test", "test");
+        refreshToken = repository.save(refreshToken);
+        logger.info("refreshToken \ncreatedTime = {}, \nmodifiedToken = {}", refreshToken.getCreatedDate(), refreshToken.getModifiedDate());
+
+        return ""+ LocalDateTime.now();
     }
 }
