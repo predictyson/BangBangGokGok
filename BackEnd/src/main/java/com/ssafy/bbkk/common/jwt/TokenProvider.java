2c884c2e07c5166bd7d64dbd52909efb1b2abf8b
smmylee212@naver.com <smmylee212>
2023-04-05 11:16:11 +0900

    ⛏ fix: 토큰 시간 재설정

--- BackEnd/src/main/java/com/ssafy/bbkk/common/jwt/TokenProvider.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/common/jwt/TokenProvider.java
@@ -26,8 +26,8 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer";
-//    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 1;            // 1분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
+//    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 1;            // 1분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 1;  // 1일
 
     private final Key key;
