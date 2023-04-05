5c27ea60b744f61c234cc502874d85cc22e4ce85
김성수 <123vfcx@gmail.com>
2023-04-05 22:45:33 +0900

    💡 refactor: 더미데이터 제거

--- FrontEnd/src/pages/MyPage/LikeThemesSection.tsx
+++ FrontEnd/src/pages/MyPage/LikeThemesSection.tsx
@@ -8,67 +8,13 @@ import { IDetailData, IReviewData } from "types/detail";
 import { getDetail } from "@/api/theme";
 import { getReviews } from "@/api/review";
 
-const initData: IDetailData = {
-  themeId: 1,
-  regionBig: "서울", // 지역(대분류)
-  regionSmall: "강남", // 지역(소분류)
-  storeName: "코드케이 홍대점", // 매장명
-  title: "", // 테마명
-  genre: ["공포", "추리"], // 장르 목록
-  difficulty: 3.2, // 난이도
-  runningTime: 60, // 시간 (분단위)
-  openDate: "2023.03.13", // 오픈일
-  minPeople: 2, // 최소 인원
-  maxPeople: 6, // 최대 인원
-  imgUrl:
-    "https://user-images.githubusercontent.com/55784772/224640336-ec8412c3-f81b-4472-b6a5-9e56254004a3.jpg", // 테마 포스터 링크
-  pageUrl: "http://www.code-k.co.kr/", // 테마 예약페이지 링크
-  synopsis: `"몇 년 전부터 조직에 잠입해 있던 언더커버로부터 대량의 마약 거래 정보가 들어왔다.
-    지휘부에서는 나를 포함한 경찰 특공대를 이 마약 조직에 몇 달 전부터 침투 시켰다.
-    오늘이 지긋지긋한 마약조직을 끝장 내버릴 마지막 기회다!"
-    `, // 테마 시놉시스
-  userRating: 3.3, // 평점
-  userActivity: 3.4, // 활동성
-  userFear: 4.4, // 공포도
-  userDifficulty: 4.4, // 체감 난이도
-  userCnt: 8, // 평가 인원
-  isInterested: false,
-};
-
-const REVIEWDUMMY: IReviewData[] = [
-  {
-    userId: 1,
-    nickname: "",
-    reviewId: 1,
-    content: "너무너무 재밌어요 눈물나요",
-    userRating: 4.2,
-    userActivity: 3.3,
-    userFear: 4.4,
-    userDifficulty: 2.2,
-    createTime: "2023-03-13",
-    isSuccess: 0, // 1: 성공 0 : 실파
-  },
-  {
-    userId: 1,
-    nickname: "",
-    reviewId: 1,
-    content: "너무너무 재밌어요 눈물나요",
-    userRating: 4.2,
-    userActivity: 3.3,
-    userFear: 4.4,
-    userDifficulty: 2.2,
-    createTime: "2023-03-13",
-    isSuccess: 1, // 1: 성공 0 : 실파
-  },
-];
-
 export default function LikeThemesSection() {
   const [interests, setInterests] = useState<UserInterestTheme[]>([]);
 
   const [open, setOpen] = useState(false);
   const [themeId, setThemeId] = useState(0);
-  const [data, setData] = useState<IDetailData>(initData);
-  const [reviews, setReviews] = useState<IReviewData[]>(REVIEWDUMMY);
+  const [data, setData] = useState<IDetailData>({} as IDetailData);
+  const [reviews, setReviews] = useState<IReviewData[]>([] as IReviewData[]);
 
   const handleOpen = async (themeId: number) => {
     setThemeId(themeId);
