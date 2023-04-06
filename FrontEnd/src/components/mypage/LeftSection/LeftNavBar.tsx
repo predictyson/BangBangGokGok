eab340ff69b20acd5ae1f283583b5c3bdce7d116
손예지 <yejison98@gmail.com>
2023-04-06 18:04:06 +0900

    💫 chore: usestate오류 해결

--- FrontEnd/src/components/mypage/LeftSection/LeftNavBar.tsx
+++ FrontEnd/src/components/mypage/LeftSection/LeftNavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { theme } from "@/styles/theme";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
