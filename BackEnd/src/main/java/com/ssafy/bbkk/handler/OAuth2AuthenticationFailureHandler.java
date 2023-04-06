b60d9f6b78af1dac36fc8e91c75e5cec9cb94b72
smmylee212@naver.com <smmylee212>
2023-04-06 16:51:59 +0900

    ⛏ fix: bean 등록이 안되던 오류 수정

--- BackEnd/src/main/java/com/ssafy/bbkk/handler/OAuth2AuthenticationFailureHandler.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/handler/OAuth2AuthenticationFailureHandler.java
@@ -1,9 +1,11 @@
 package com.ssafy.bbkk.handler;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Slf4j
+@Component
+@RequiredArgsConstructor
 public class OAuth2AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
 
     @Override
