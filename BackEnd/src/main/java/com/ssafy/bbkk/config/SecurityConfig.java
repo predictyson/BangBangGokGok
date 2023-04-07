d9ddcbb51ef8ff48d6b9bb1ba1f7337cfbbad09c
smmylee212@naver.com <smmylee212>
2023-04-07 09:36:12 +0900

    ⛏ fix: 접근 권한 수정

--- BackEnd/src/main/java/com/ssafy/bbkk/config/SecurityConfig.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/config/SecurityConfig.java
@@ -70,15 +70,15 @@ public class SecurityConfig{
         .and()
             .authorizeRequests()
             .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-            .antMatchers("/user/oauth/login",
-                    "/user/check/login/**",
-                    "/profile/**",
-                    "/theme/user/**",
-                    "/theme/**/user",
-                    "/interest/**",
-                    "/review/**",
-                    "/groupset/**"
-                    ).authenticated()
+//            .antMatchers("/user/oauth/login",
+//                    "/user/check/login/**",
+//                    "/profile/**",
+//                    "/theme/user/**",
+//                    "/theme/**/user",
+//                    "/interest/**",
+//                    "/review/**",
+//                    "/groupset/**"
+//                    ).authenticated()
             .antMatchers(PERMIT_URL_ARRAY).permitAll()
             .anyRequest().permitAll()
 
