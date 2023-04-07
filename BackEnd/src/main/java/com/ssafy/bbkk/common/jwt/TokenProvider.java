22fcdb1e03bd782d8a8d84748344916c1f280bbc
smmylee212@naver.com <smmylee212>
2023-04-07 09:17:35 +0900

    ⛏ fix: 토큰 유효 기간 설정

--- BackEnd/src/main/java/com/ssafy/bbkk/common/jwt/TokenProvider.java
+++ BackEnd/src/main/java/com/ssafy/bbkk/common/jwt/TokenProvider.java
@@ -26,7 +26,7 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 1;            // 30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
 //    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 10;            // 10시간
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 1;  // 1일
 
